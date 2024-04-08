441c657b66ad9d6e2121402599571be27a6ab9f5
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-08 20:41:29 +0900

    :dizzy:[update]selector rename

--- js/components/GamePlay.js
+++ js/components/GamePlay.js
@@ -11,9 +11,9 @@ export default class extends PageBase {
     async getHtml() {
         return `
            <div class="playBoardWrap">
-                <ul class="listPlayer">
-                    <li class="listPlayer_item">player1</li>
-                    <li class="listPlayer_item">player2</li>
+                <ul class="listPlayerActiveMatch">
+                    <li class="listPlayerActiveMatch_item">player1</li>
+                    <li class="listPlayerActiveMatch_item">player2</li>
                 </ul>
                 <canvas id="playBoard"></canvas>
             </div>
