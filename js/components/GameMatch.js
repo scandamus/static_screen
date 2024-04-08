97345e09b99420f76f129ac889e153ff9ea8400c
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-08 20:41:13 +0900

    :sparkles:[add]gameMatch

--- js/components/GameMatch.js
+++ js/components/GameMatch.js
@@ -10,7 +10,73 @@ export default class extends PageBase {
 
     async getHtml() {
         return `
-            <p>GameMatch</p>
+            <div class="listPlayers">
+                <section class="listPlayers_item unitPlayer">
+                    <header class="unitPlayer_header unitPlayerHeader">
+                        <div class="unitPlayerHeader_name">
+                            <h3>user1</h3>
+                            <p class="thumb"><img src="https://dummyimage.com/100x100/59fa14/212121.png&text=ico" alt="" width="60" height="60"></p>
+                        </div>
+                        <button class="unitPlayerHeader_button">対戦を申し込む</button>
+                    </header>
+                    <div class="unitPlayer_score">
+                        <p>RANK: 4 (20勝3敗)</p>
+                        <p>過去の対戦成績: 2勝1敗</p>
+                    </div>
+                </section>
+                <section class="listPlayers_item unitPlayer">
+                    <header class="unitPlayer_header unitPlayerHeader">
+                        <div class="unitPlayerHeader_name">
+                            <h3>user1</h3>
+                            <p class="thumb"><img src="https://dummyimage.com/100x100/59fa14/212121.png&text=ico" alt="" width="60" height="60"></p>
+                        </div>
+                        <button class="unitPlayerHeader_button">対戦を申し込む</button>
+                    </header>
+                    <div class="unitPlayer_score">
+                        <p>RANK: 4 (20勝3敗)</p>
+                        <p>過去の対戦成績: 2勝1敗</p>
+                    </div>
+                </section>
+                <section class="listPlayers_item unitPlayer">
+                    <header class="unitPlayer_header unitPlayerHeader">
+                        <div class="unitPlayerHeader_name">
+                            <h3>user1</h3>
+                            <p class="thumb"><img src="https://dummyimage.com/100x100/59fa14/212121.png&text=ico" alt="" width="60" height="60"></p>
+                        </div>
+                        <button class="unitPlayerHeader_button">対戦を申し込む</button>
+                    </header>
+                    <div class="unitPlayer_score">
+                        <p>RANK: 4 (20勝3敗)</p>
+                        <p>過去の対戦成績: 2勝1敗</p>
+                    </div>
+                </section>
+                <section class="listPlayers_item unitPlayer">
+                    <header class="unitPlayer_header unitPlayerHeader">
+                        <div class="unitPlayerHeader_name">
+                            <h3>user1</h3>
+                            <p class="thumb"><img src="https://dummyimage.com/100x100/59fa14/212121.png&text=ico" alt="" width="60" height="60"></p>
+                        </div>
+                        <button class="unitPlayerHeader_button">対戦を申し込む</button>
+                    </header>
+                    <div class="unitPlayer_score">
+                        <p>RANK: 4 (20勝3敗)</p>
+                        <p>過去の対戦成績: 2勝1敗</p>
+                    </div>
+                </section>
+                <section class="listPlayers_item unitPlayer">
+                    <header class="unitPlayer_header unitPlayerHeader">
+                        <div class="unitPlayerHeader_name">
+                            <h3>user1</h3>
+                            <p class="thumb"><img src="https://dummyimage.com/100x100/59fa14/212121.png&text=ico" alt="" width="60" height="60"></p>
+                        </div>
+                        <button class="unitPlayerHeader_button">対戦を申し込む</button>
+                    </header>
+                    <div class="unitPlayer_score">
+                        <p>RANK: 4 (20勝3敗)</p>
+                        <p>過去の対戦成績: 2勝1敗</p>
+                    </div>
+                </section>
+            </div>
         `;
     }
 }
