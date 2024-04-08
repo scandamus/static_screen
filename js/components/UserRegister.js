8e299c35aef2deab1a88a499af08d00fe211671e
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-09 01:21:00 +0900

    :sparkles:[add]userRegister

--- js/components/UserRegister.js
+++ js/components/UserRegister.js
@@ -5,12 +5,27 @@ import PageBase from "./PageBase.js";
 export default class extends PageBase {
     constructor(params) {
         super(params);
-        this.setTitle("UserRegister.js");
+        this.setTitle("UserRegister");
+        this.labelButton = "登録する";
     }
 
     async getHtml() {
         return `
-            <p>UserRegister.js</p>
+            <form class="formUserResister blockForm" action="" method="post">
+                <dl class="unitFormInput">
+                    <dt class="unitFormInput_label"><label for="">name</label></dt>
+                    <dd class="unitFormInput_input"><input type="text" /></dd>
+                </dl>
+                <dl class="unitFormInput">
+                    <dt class="unitFormInput_label"><label for="">email</label></dt>
+                    <dd class="unitFormInput_input"><input type="text" /></dd>
+                </dl>
+                <dl class="unitFormInput">
+                    <dt class="unitFormInput_label"><label for="">password</label></dt>
+                    <dd class="unitFormInput_input"><input type="password" /></dd>
+                </dl>
+                <button type="submit" id="btnLogin" class="formUserResister_button unitButton unitButton-large">${this.labelButton}</button>
+            </form>
         `;
     }
 }
