1de3de99842f2c1fcbf1065093ab4f16247c771c
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-11 03:48:37 +0900

    :dizzy:[update]game playerを変数に

--- js/components/GamePlay.js
+++ js/components/GamePlay.js
@@ -6,14 +6,16 @@ export default class extends PageBase {
     constructor(params) {
         super(params);
         this.setTitle("GamePlay");
+        this.player1 = "player1人目";
+        this.player2 = "player2人目";
     }
 
     async getHtml() {
         return `
            <div class="playBoardWrap">
                 <ul class="listPlayerActiveMatch">
-                    <li class="listPlayerActiveMatch_item">player1</li>
-                    <li class="listPlayerActiveMatch_item">player2</li>
+                    <li class="listPlayerActiveMatch_item">${this.player1}</li>
+                    <li class="listPlayerActiveMatch_item">${this.player2}</li>
                 </ul>
                 <canvas id="playBoard"></canvas>
             </div>
