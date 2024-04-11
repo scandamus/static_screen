d0563a19b7b477ae2b4ac310edb99de5b7709074
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-11 15:05:42 +0900

    :sparkles:[add]TournamentEntry
    list 共通化

--- js/components/GameMatch.js
+++ js/components/GameMatch.js
@@ -12,67 +12,67 @@ export default class extends PageBase {
     async getHtml() {
         return `
             <div class="listPlayers">
-                <section class="listPlayers_item unitPlayer">
-                    <header class="unitPlayer_header unitPlayerHeader">
-                        <div class="unitPlayerHeader_name">
+                <section class="unitListItem">
+                    <header class="unitListItem_header">
+                        <div class="unitPlayerName">
                             <h3>user1</h3>
                             <p class="thumb"><img src="//ui-avatars.com/api/?name=Aa Bb&background=e3ad03&color=ffffff" alt="" width="60" height="60"></p>
                         </div>
-                        <button class="unitPlayerHeader_button unitButton">${this.labelButton}</button>
+                        <button class="unitButton">${this.labelButton}</button>
                     </header>
-                    <div class="unitPlayer_score">
+                    <div class="unitListItem_content unitPlayerScore">
                         <p>RANK: 4 (20勝3敗)</p>
                         <p>過去の対戦成績: 2勝1敗</p>
                     </div>
                 </section>
-                <section class="listPlayers_item unitPlayer">
-                    <header class="unitPlayer_header unitPlayerHeader">
-                        <div class="unitPlayerHeader_name">
+                <section class="unitListItem">
+                    <header class="unitListItem_header">
+                        <div class="unitPlayerName">
                             <h3>user1</h3>
                             <p class="thumb"><img src="//ui-avatars.com/api/?name=Cc Dd&background=59fa14&color=ffffff" alt="" width="60" height="60"></p>
                         </div>
-                        <button class="unitPlayerHeader_button unitButton">${this.labelButton}</button>
+                        <button class="unitButton">${this.labelButton}</button>
                     </header>
-                    <div class="unitPlayer_score">
+                    <div class="unitListItem_content unitPlayerScore">
                         <p>RANK: 4 (20勝3敗)</p>
                         <p>過去の対戦成績: 2勝1敗</p>
                     </div>
                 </section>
-                <section class="listPlayers_item unitPlayer">
-                    <header class="unitPlayer_header unitPlayerHeader">
-                        <div class="unitPlayerHeader_name">
+                <section class="unitListItem">
+                    <header class="unitListItem_header">
+                        <div class="unitPlayerName">
                             <h3>user1</h3>
                             <p class="thumb"><img src="//ui-avatars.com/api/?name=Ee Ff&background=2e99f0&color=ffffff" alt="" width="60" height="60"></p>
                         </div>
-                        <button class="unitPlayerHeader_button unitButton">${this.labelButton}</button>
+                        <button class="unitButton">${this.labelButton}</button>
                     </header>
-                    <div class="unitPlayer_score">
+                    <div class="unitListItem_content unitPlayerScore">
                         <p>RANK: 4 (20勝3敗)</p>
                         <p>過去の対戦成績: 2勝1敗</p>
                     </div>
                 </section>
-                <section class="listPlayers_item unitPlayer">
-                    <header class="unitPlayer_header unitPlayerHeader">
-                        <div class="unitPlayerHeader_name">
+                <section class="unitListItem">
+                    <header class="unitListItem_header">
+                        <div class="unitPlayerName">
                             <h3>user1</h3>
                             <p class="thumb"><img src="//ui-avatars.com/api/?name=Gg Hh&background=872bac&color=ffffff" alt="" width="60" height="60"></p>
                         </div>
-                        <button class="unitPlayerHeader_button unitButton">${this.labelButton}</button>
+                        <button class="unitButton">${this.labelButton}</button>
                     </header>
-                    <div class="unitPlayer_score">
+                    <div class="unitListItem_content unitPlayerScore">
                         <p>RANK: 4 (20勝3敗)</p>
                         <p>過去の対戦成績: 2勝1敗</p>
                     </div>
                 </section>
-                <section class="listPlayers_item unitPlayer">
-                    <header class="unitPlayer_header unitPlayerHeader">
-                        <div class="unitPlayerHeader_name">
+                <section class="unitListItem">
+                    <header class="unitListItem_header">
+                        <div class="unitPlayerName">
                             <h3>user1</h3>
                             <p class="thumb"><img src="//ui-avatars.com/api/?name=Ii Jj&background=ff0000&color=ffffff" alt="" width="60" height="60"></p>
                         </div>
-                        <button class="unitPlayerHeader_button unitButton">${this.labelButton}</button>
+                        <button class="unitButton">${this.labelButton}</button>
                     </header>
-                    <div class="unitPlayer_score">
+                    <div class="unitListItem_content unitPlayerScore">
                         <p>RANK: 4 (20勝3敗)</p>
                         <p>過去の対戦成績: 2勝1敗</p>
                     </div>
